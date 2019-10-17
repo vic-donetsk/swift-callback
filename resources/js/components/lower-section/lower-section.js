ae31737eb9e63b762639505a0bcc37a7007381cc
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-10-17 22:05:51 +0300

    -add single component "lower-section", move it from viber page

--- resources/js/components/lower-section/lower-section.js
+++ resources/js/components/lower-section/lower-section.js
@@ -2,5 +2,30 @@ export default {
     data: function () {
         return {}
     },
-    methods: {},
+    methods: {
+        call() {
+            SwiftWidgetEventBus.$emit('go', 'page-call');
+        },
+        chat() {
+            SwiftWidgetEventBus.$emit('go', 'page-chat');
+        },
+        messenger() {
+            SwiftWidgetEventBus.$emit('go', 'page-messenger');
+        },
+        telegram() {
+            SwiftWidgetEventBus.$emit('go', 'page-telegram');
+        },
+        vk() {
+            SwiftWidgetEventBus.$emit('go', 'page-vk');
+        },
+        viber() {
+            SwiftWidgetEventBus.$emit('go', 'page-viber');
+        },
+        whatsapp() {
+            SwiftWidgetEventBus.$emit('go', 'page-whatsapp');
+        },
+        mail() {
+            SwiftWidgetEventBus.$emit('go', 'page-mail');
+        },
+    },
 }
--- resources/js/pages/viber/viber.js
+++ resources/js/pages/viber/viber.js
@@ -8,30 +8,6 @@ export default {
         return {}
     },
     methods: {
-        call() {
-            SwiftWidgetEventBus.$emit('go', 'page-call');
-        },
-        chat() {
-            SwiftWidgetEventBus.$emit('go', 'page-chat');
-        },
-        messenger() {
-            SwiftWidgetEventBus.$emit('go', 'page-messenger');
-        },
-        telegram() {
-            SwiftWidgetEventBus.$emit('go', 'page-telegram');
-        },
-        vk() {
-            SwiftWidgetEventBus.$emit('go', 'page-vk');
-        },
-        viber() {
-            SwiftWidgetEventBus.$emit('go', 'page-viber');
-        },
-        whatsapp() {
-            SwiftWidgetEventBus.$emit('go', 'page-whatsapp');
-        },
-        mail() {
-            SwiftWidgetEventBus.$emit('go', 'page-mail');
-        },
         exit() {
             SwiftWidgetEventBus.$emit('go','widget-button');
         }
