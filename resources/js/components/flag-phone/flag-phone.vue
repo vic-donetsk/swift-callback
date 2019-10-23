b58e655f0da4103dc626e6d3fbdc26863dc22862
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-10-23 08:02:51 +0300

    -test

--- resources/js/components/flag-phone/flag-phone.vue
+++ resources/js/components/flag-phone/flag-phone.vue
@@ -1,16 +1,46 @@
 <template>
     <div class="swift-widget_flagPhone">
-        <label for="select" class="swift-widget_flagPhone-select">
-            <input type="radio" name="list" value="not_changed" id="bg" checked />
-            <label class="bg" for="bg"><img src="img/flags/moldova.svg" alt=""></label>
-            <input type="radio" name="list" value="not_changed" id="select">
-            <div class="items">
-                <div v-if="countriesPhones.length"  v-for="item in countriesPhones">
-                    <input type="radio" name="list" :value="item.id" :id="item.id" v-model="selected" @change="flagSelected">
-                    <label :for="item.id"><img :src="item.img" alt="tel" ></label>
+<!--        <label for="select" class="swift-widget_flagPhone-select">-->
+<!--            <input type="radio" name="list" value="not_changed" id="bg" checked />-->
+<!--            <label class="bg" for="bg"><img src="img/flags/moldova.svg" alt=""></label>-->
+<!--            <input type="radio" name="list" value="not_changed" id="select">-->
+<!--            <div class="items">-->
+<!--                <div v-if="countriesPhones.length"  v-for="item in countriesPhones">-->
+<!--                    <input type="radio" name="list" :value="item.id" :id="item.id" v-model="selected" @change="flagSelected">-->
+<!--                    <label :for="item.id"><img :src="item.img" alt="" ></label>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </label>-->
+
+        <cool-select
+                v-model="selectedTimezone"
+                :items="items"
+                :placeholder="selectedTimezone ? '' : 'Select timezone'"
+                item-value="name"
+                item-text="name"
+        >
+            <!-- slot for each item in the menu -->
+            <template slot="item" slot-scope="{ item: timezone }">
+                <div style="display: flex; align-items: center;">
+                    <img src="/img/moldova.svg" class="country-flag">
+<!--                    :src="getFlag(timezone.countryCode)"-->
+                    <div>
+                        {{ timezone.offset }}
+                        <b>{{ timezone.name }}</b>
+                    </div>
                 </div>
-            </div>
-        </label>
+            </template>
+            <!-- slot for the selected item (in the text field) -->
+            <template slot="selection" slot-scope="{ item: timezone }">
+                <img src="/img/moldova.svg" class="country-flag">
+
+                <div>
+                    {{ timezone.offset }}
+                    <b>{{ timezone.name }}</b>
+                </div>
+            </template>
+        </cool-select>
+
         <masked-input class="swift-widget_flagPhone-input"
                       v-model="clientPhone"
                       :mask="currentMask"
