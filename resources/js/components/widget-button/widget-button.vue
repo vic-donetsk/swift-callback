275c3c15d8a8fbf0d4374857f09e2a3cfa3f6490
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-11-16 22:18:51 +0200

    -fix resize, -start mobile button

--- resources/js/components/widget-button/widget-button.vue
+++ resources/js/components/widget-button/widget-button.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="swift-widget-button" @click="open">
+    <div class="swift-widget-button_wrapper">
+        <div class="swift-widget-button" @click="open">
 
-        <div id="swift-widget-button_circle-1" :class="{mod_visible: visibleButton}">
-            <img src="img/pulse-button/ellipse_1.svg"alt="SWIFT Button">
+            <div id="swift-widget-button_circle-1" :class="{mod_visible: visibleButton}">
+                <img src="img/pulse-button/ellipse_1.svg" alt="SWIFT Button">
+            </div>
+            <div id="swift-widget-button_circle-2" class="swift-widget-circle" v-if="visibleCircles"></div>
+            <div id="swift-widget-button_circle-3" class="swift-widget-circle" v-if="visibleCircles"></div>
+            <div id="swift-widget-button_circle-4" class="swift-widget-circle" v-if="visibleCircles"></div>
+            <div id="swift-widget-button_circle-5" class="swift-widget-circle" v-if="visibleCircles"></div>
+
         </div>
-        <div id="swift-widget-button_circle-2" class="swift-widget-circle" v-if="visibleCircles"></div>
-        <div id="swift-widget-button_circle-3" class="swift-widget-circle" v-if="visibleCircles"></div>
-        <div id="swift-widget-button_circle-4" class="swift-widget-circle" v-if="visibleCircles"></div>
-        <div id="swift-widget-button_circle-5" class="swift-widget-circle" v-if="visibleCircles"></div>
-
     </div>
 </template>
 
